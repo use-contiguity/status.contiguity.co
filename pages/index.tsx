319864859db2367e2dcda1eccda05574b03d96a8
Oxbridge <hello@def-not-hacking-the.net>
2023-11-29 13:29:59 -0500

    More more fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,22 @@
-import type { NextPage } from 'next';
-import Image from 'next/image';
-import IncidentsSection from '../src/incidents';
-import ServicesSection from '../src/services';
+import type { NextPage } from 'next'
+import Image from 'next/image'
+import IncidentsSection from "../src/incidents"
+import ServicesSection from "../src/services"
 
 const Home: NextPage = () => {
   return (
-    <div className='h-screen relative'>
-      <div className="absolute inset-0 bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
-      <div className="w-full h-40 bg-black flex items-center justify-center">
-        <div>
-          <Image src="/logo.png" width={250} height={50} style={{marginTop: '20px'}} className="w-40 h-16" alt="Tailwind Play" />
+    <div className='h-full w-full '>
+      <div className="mt-20 absolute inset-0 bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
+      <div className="w-full h-40 absolute bg-black">
+        <div className="sm:ml-0 ml-5 mr-0 mt-3 md:pl-80 md:pr-80 sm:w-full h-full bg-black">
+          <Image src="/logo.png" width={200} height={40} className="w-40 h-16" alt="Tailwind Play" />
         </div>
       </div>
-      <div className='mt-20 w-full overflow-scroll'>
+      <div className='mt-20 w-full absolute overflow-scroll	'>
         <ServicesSection />
-      </div>
+      </div >
     </div>
-  );
-};
+  )
+}
 
 export default Home;
